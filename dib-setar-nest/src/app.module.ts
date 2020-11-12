b258bfd0541357e1754e8b606ab74193b6e8fee3
Alexey Shumlyaev <alexey.s@upland.me>
2020-11-13 00:04:39 +0200

    added external mongo url

--- dib-setar-nest/src/app.module.ts
+++ dib-setar-nest/src/app.module.ts
@@ -4,8 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BidsModule } from './bids/bids.module';
 
+const mongoUrl = encodeURI('mongodb+srv://mongo_user:Temp1234%@brightech-volunteer.dv1tl.mongodb.net/nest?retryWrites=true&w=majority&authSource=admin');
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/dib-setar'), BidsModule],
+  imports: [MongooseModule.forRoot(mongoUrl), BidsModule],
   controllers: [AppController],
   providers: [AppService],
 })
